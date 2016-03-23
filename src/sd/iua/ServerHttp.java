539f8830aa66c76f8435300604f331c7f4c0286d
Francisco Giana <gianafrancisco@gmail.com>
2016-03-23 13:42:47 -0300

    added support for response 500 when the max concurrent connections have been reached.

--- src/sd/iua/ServerHttp.java
+++ src/sd/iua/ServerHttp.java
@@ -1,15 +1,22 @@
 package sd.iua;
 
+import sd.iua.model.HttpResponse;
+import sd.iua.model.HttpResponseImpl;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class ServerHttp {
 
 	private static Properties prop = new Properties();
+	private static List<AtiendeCliente> clientes;
+	private static int maxConn;
 
 	public static void main(String[] args) throws IOException {
 		prop.load(new FileInputStream(new File(System.getProperty("user.dir"), "httpServer.properties")));
@@ -19,6 +26,10 @@ public class ServerHttp {
 			System.err.printf("ERROR: '%s' no es una ruta correcta.%n", folder.getAbsolutePath());
 			System.exit(-1);
 		}
+		maxConn = Integer.parseInt(prop.getProperty("httpserver.maxconn", "10"));
+		clientes = new ArrayList<>();
+
+
 
 		@SuppressWarnings("resource")
 		ServerSocket server = new ServerSocket(puertoServer);
@@ -27,8 +38,19 @@ public class ServerHttp {
 
 		while (true) {
 			Socket cliente = server.accept();
-			System.out.println(cliente.getPort() + " conectado.");
-			new AtiendeCliente(cliente, prop, folder).start();
+			for(Thread t: clientes){
+				if(!t.isAlive()){clientes.remove(t); break;};
+			}
+			if(clientes.size() < maxConn) {
+				System.out.println(cliente.getPort() + " conectado.");
+				AtiendeCliente atiendeCliente = new AtiendeCliente(cliente, prop, folder);
+				clientes.add(atiendeCliente);
+				atiendeCliente.start();
+			}else{
+				System.err.println("ERROR: se ha llegado a la capacidad maxima de clientes conectados.");
+				cliente.getOutputStream().write(new HttpResponseImpl().getStatus500().getBytes());
+				cliente.close();
+			}
 		}
 
 	}
